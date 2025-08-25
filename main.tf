b8fb5b462c74a1a9266a4873421cc4ae14eb7a53
Kyler Dana <kymasterbox@gmail.com>
2025-08-25 15:10:07 -0700

    Added Public Route Table + Association

--- main.tf
+++ main.tf
@@ -107,5 +107,25 @@ resource "aws_nat_gateway" "nat" {
     tags = {Name = "${var.project}-nat"}
 }
 
+# Public Route Table 
+# Default route to Internet via Internet Gateway
+resource "aws_route_table" "public" {
+    vpc_id = aws_vpc.main.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.igw.id 
+    }
+    tags = {Name = "${var.project}-public-rt"}
+}
+
+# Public Route Table Association
+# Public Subnet -> Public Route Table
+resource "aws_route_table_association" "public_assoc" {
+    count = length(aws_subnet.public)
+    subnet_id = aws_subnet.public[count.index].id
+    route_table_id = aws_route_table.public.id
+}
+
 
 
