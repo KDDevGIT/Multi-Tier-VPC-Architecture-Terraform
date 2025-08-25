43f51cb512fb10bcb0d88c00767b810ce5623415
Kyler Dana <kymasterbox@gmail.com>
2025-08-25 14:42:49 -0700

    Added 2 Public Subnets for failover, Dynamic IP on start.

--- main.tf
+++ main.tf
@@ -60,3 +60,20 @@ resource "aws_internet_gateway" "igw" {
     }
 }
 
+# Mutliple Availability Zones 
+# 2 Public and 2 Private Subnets for failover
+
+# Public Subnet(s) (Dynamic IP)
+# Split VPC CIDR into chunks
+resource "aws_subnet" "public" {
+    count = 2
+    vpc_id = aws_vpc.main.id
+    cidr_block = cidrsubnet(var.vpc_cidr,4,count.index) #10.0.0.0/24, 10.0.1.0/24....
+    availability_zone = data.aws_availability_zones.available.names[count.index]
+    map_public_ip_on_launch = true
+
+    tags = {
+        Name = "${var.project}-public-${count.index + 1}"
+        Tier = "public"
+    }
+}
