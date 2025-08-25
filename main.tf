4cd8995a55f2900bc5b19f08f5fe161d302e8115
Kyler Dana <kymasterbox@gmail.com>
2025-08-25 14:31:28 -0700

    Added VPN with DNS Support.

--- main.tf
+++ main.tf
@@ -37,3 +37,16 @@ data "aws_ami" "amazon_linux" {
 
 # Networking
 # Includes Subnets, Gateways, and Routes
+
+# VPC with DNS Support (CIDR)
+# Default CIDR is 10.0.0.0/20
+resource "aws_vpc" "main" {
+    cidr_block           = var.vpc_cidr
+    enable_dns_hostnames = true
+    enable_dns_support   = true
+
+    tags = {
+        Name = "${var.project}-vpc"
+        Env  = var.env
+    }
+}
