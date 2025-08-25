8633a34e59e109c346264092c5d97d7956a42e6a
Kyler Dana <kymasterbox@gmail.com>
2025-08-25 15:18:52 -0700

    Added Private Route Table + Association

--- main.tf
+++ main.tf
@@ -127,5 +127,27 @@ resource "aws_route_table_association" "public_assoc" {
     route_table_id = aws_route_table.public.id
 }
 
+# Private Route Table
+# Can reach internet without exposure
+# Routes to NAT for egress
+resouece "aws_route_table" "private" {
+    vpc_id = aws_vpc.main.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        nat_gateway_id = aws_nat_gateway.nat.id
+    }
+    tags = {Name = "${var.project}-private-rt"}
+}
+
+# Private Route Table Association
+# Private Subnet -> Private Route Table
+resource "aws_route_table_association" "private_assoc" {
+    count = length(aws_subnet.private)
+    subnet_id = aws_subnet.private[count.index].id
+    route_table_id = aws_route_table.private.id
+}
+
+
 
 
